3dabcd009aec3141ff79e4e27965ea5fe6b5e294
pizeroLOL <71167373+pizeroLOL@users.noreply.github.com>
2022-04-14 21:27:00 +0800

    Update start.sh

--- 使用/开始/start.sh
+++ 使用/开始/start.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 sudo apt update -y
 sudo apt upgrade -y
-sudo apt install openjdk-8-jdk git -y
-git clone https://github.com/pizeroLOL/mc-1.12.2-auto.git
-sh ./mc-1.12.2-auto/gradlew build 
+sudo apt install openjdk-8-jdk -y
+sh ./mc-1.12.2-auto/gradlew build
