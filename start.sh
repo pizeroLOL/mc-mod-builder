b7d9e6b78bcc92cd7b3d38373394fc7521e045bb
pizeroLOL <71167373+pizeroLOL@users.noreply.github.com>
2022-04-15 17:16:23 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ sudo apt upgrade -y
 sudo apt install openjdk-8-jdk wget -y
 wget https://github.com/sinkillerj/ProjectE/archive/refs/heads/mc1.12.x.zip
 unzip mc1.12.x.zip
-sh ./Applied-Energistics-2/gradlew build
+sh ./ProjectE-mc1.12.x/gradlew build
