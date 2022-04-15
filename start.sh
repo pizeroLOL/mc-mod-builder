067aedaa2f1913c83ea4d3fd66013d3b3b6b781e
pizeroLOL <71167373+pizeroLOL@users.noreply.github.com>
2022-04-15 18:36:46 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@ sudo apt update -y
 sudo apt upgrade -y
 sudo apt install openjdk-8-jdk wget -y
 wget https://github.com/GregTechCE/GregTech/archive/refs/heads/master.zip
-unzip master.x.zip
+unzip master.zip
 cd GregTech-master
 sh ./gradlew build -Dfile.encoding=UTF-8 --info
