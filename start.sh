0094858aa28d5d552a978435ce0a7b3bb4d7c28b
pizeroLOL <71167373+pizeroLOL@users.noreply.github.com>
2022-04-15 18:54:49 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt install openjdk-8-jdk wget -y
-wget https://github.com/GregTechCE/GregTech/archive/refs/heads/master.zip
-unzip master.zip
-cd GregTech-master
+wget https://github.com/Dabai1025/PaiMonFood/archive/refs/heads/main.zip
+unzip main.zip
+cd PaiMonFood-main
 sh ./gradlew build -Dfile.encoding=UTF-8 --info
