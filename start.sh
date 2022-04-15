c464c763d663a097484fe481c2ad42c9d4a702de
pizeroLOL <71167373+pizeroLOL@users.noreply.github.com>
2022-04-15 15:20:04 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ sudo apt upgrade -y
 sudo apt install openjdk-8-jdk wget -y
 wget https://github.com/Blood-Asp/GT5-Unofficial/archive/refs/heads/unstable.zip
 unzip unstable.zip
-sh ./gradlew build
+sh ./GT5-Unofficial-unstable/gradlew build
