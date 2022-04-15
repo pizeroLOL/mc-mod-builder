ace134d965dc3b5009917bcb1f4a1a4bb7f86fdc
pizeroLOL <71167373+pizeroLOL@users.noreply.github.com>
2022-04-15 22:42:37 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,5 +2,8 @@
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt install openjdk-8-jdk wget -y
-wget <你的网址（记得吧<>删掉）>
-sh ./upload/gradlew build
+wget $DL_URL
+unzip $Pack_Name.zip
+cd $Open_Pack_Name
+sh ./gradlew build -Dfile.encoding=UTF-8 --info
+cp -r build/libs /opt/libs
