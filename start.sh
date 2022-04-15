220f77e68552538d20d69951ece17068ad6d13dc
pizeroLOL <71167373+pizeroLOL@users.noreply.github.com>
2022-04-15 18:24:43 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,4 +4,5 @@ sudo apt upgrade -y
 sudo apt install openjdk-8-jdk wget -y
 wget https://github.com/sinkillerj/ProjectE/archive/refs/heads/mc1.12.x.zip
 unzip mc1.12.x.zip
-sh ./ProjectE-mc1.12.x/gradlew build
+cd ProjectE-mc1.12.x
+sh ./gradlew build
