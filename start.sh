d086f7d4b452a6ac6b8bfac0ed68af0eb515839c
pizeroLOL <71167373+pizeroLOL@users.noreply.github.com>
2022-05-20 01:17:22 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,11 +2,11 @@
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt install wget -y
-wget $DL_URL
-unzip $Pack_Name.zip
-cd $Open_Pack_Name
+wget https://dl.google.com/android/repository/commandlinetools-linux-8512546_latest.zip
+unzip commandlinetools-linux-8512546_latest.zip
+cd cmdline-tools/bin
 export JAVA11_HOME=/opt/hostedtoolcache/Java_Temurin-Hotspot_jdk/11.0.15-10/x64
 export JAVA18_HOME=/opt/hostedtoolcache/Java_Temurin-Hotspot_jdk/18.0.1-10/x64
 export JAVA8_HOME=/opt/hostedtoolcache/Java_Temurin-Hotspot_jdk/8.0.332-9/x64
-sh ./gradlew build -Dfile.encoding=UTF-8 --info
-cp -r build/libs /opt/libs
+sh ./sdkmanager --sdk_root=/opt/android-sdk/ --list > log.txt
+cp -r log.txt /opt/libs
