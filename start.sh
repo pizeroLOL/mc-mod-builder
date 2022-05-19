b5314c3ba2c3de970c7698d2e5359fea190527a2
pizeroLOL <71167373+pizeroLOL@users.noreply.github.com>
2022-05-20 01:31:48 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,5 +8,8 @@ cd cmdline-tools/bin
 export JAVA11_HOME=/opt/hostedtoolcache/Java_Temurin-Hotspot_jdk/11.0.15-10/x64
 export JAVA18_HOME=/opt/hostedtoolcache/Java_Temurin-Hotspot_jdk/18.0.1-10/x64
 export JAVA8_HOME=/opt/hostedtoolcache/Java_Temurin-Hotspot_jdk/8.0.332-9/x64
-sh ./sdkmanager --sdk_root=/opt/android-sdk/ --list > log.txt
+export JAVA_HOME=$JAVA_HOME18
+sh ./sdkmanager --sdk_root=/opt/android-sdk/ "build-tools;32.0.0"
+sh ./sdkmanager --sdk_root=/opt/android-sdk/ "platforms;android-32"
+sh ./sdkmanager --sdk_root=/opt/android-sdk/ "add-ons;addon-google_apis-google-24"
 cp -r log.txt /opt/libs
