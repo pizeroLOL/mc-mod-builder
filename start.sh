b74af722d8a83c638a1e491b0f90b1dc9b831635
pizeroLOL <71167373+pizeroLOL@users.noreply.github.com>
2022-06-01 02:15:26 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,21 +1,13 @@
 #!/bin/sh
 sudo apt update -y
 sudo apt upgrade -y
+sudo apt install openjdk-8-jdk openjdk-11-jre openjdk-18-jdk wget -y
 sudo apt install wget -y
-wget https://dl.google.com/android/repository/commandlinetools-linux-8512546_latest.zip
-unzip commandlinetools-linux-8512546_latest.zip
-cd cmdline-tools/bin
+wget $DL_URL
+unzip $Pack_Name.zip
+cd $Open_Pack_Name
 export JAVA11_HOME=/opt/hostedtoolcache/Java_Temurin-Hotspot_jdk/11.0.15-10/x64
 export JAVA18_HOME=/opt/hostedtoolcache/Java_Temurin-Hotspot_jdk/18.0.1-10/x64
 export JAVA8_HOME=/opt/hostedtoolcache/Java_Temurin-Hotspot_jdk/8.0.332-9/x64
-export JAVA_HOME=$JAVA_HOME18
-sh ./sdkmanager --sdk_root=/opt/android-sdk/ "build-tools;32.0.0"
-sh ./sdkmanager --sdk_root=/opt/android-sdk/ "platforms;android-32"
-sh ./sdkmanager --sdk_root=/opt/android-sdk/ "add-ons;addon-google_apis-google-24"
-yes | ./sdkmanager --licenses
-export ANDROID_SDK_ROOT=/opt/android-sdk/
-wget https://github.com/AOF-Dev/MCinaBox/archive/refs/heads/dev.zip
-unzip dev.zip
-cd MCinaBox-dev
-./gradlew assembleDebug
-cp -rf ./app/lib /opt/libs
+sh ./gradlew build -Dfile.encoding=UTF-8 --info
+cp -r build/libs /opt/libs
